b7061d6d9a747690fd963a22b5b0c4a55ef79a0d
libowen <1064416560@qq.com>
2018-03-23 19:04:52 +0800

    进度条Demo

---
+++ 进度条/实时获取数据类/shishi.js
@@ -0,0 +1,20 @@
+$(function(){
+	var img = $("img");
+	var num = 0;
+
+	img.each(function(i){
+		var oImg = new Image();
+
+		oImg.onload = function(){
+			num++;
+
+			$(".loading b").html(parseInt(num/$("img").size()*100)+'%');
+			if (num == i) {
+				$(".loading").fadeOut();
+			}
+		}
+		oImg.src=img[i].src;
+
+	});
+
+})
